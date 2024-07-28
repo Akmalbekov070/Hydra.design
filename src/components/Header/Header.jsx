fc2007d869d3d0b5a0312f0f891c07059ab1d813
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-07-28 22:49:28 +0300

    Header page design upload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,38 @@
-import { Box, Image } from '@chakra-ui/react';
+import { Box, Button, Heading, HStack, Image } from '@chakra-ui/react';
+import Link from 'next/link';
 import React from 'react';
 
 export default function Header() {
-	return <Box>{/* <Image src={'https://i.postimg.cc/DwCXvRRt/Frame.png'} alt='Logo' /> */}</Box>;
+	return (
+		<Box w={'full'} display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
+			<HStack>
+				<Image src={'https://i.postimg.cc/DwCXvRRt/Frame.png'} alt='Logo' />
+				<Heading
+					bgGradient='linear(to-l, #C0B7E8, #8176AF)'
+					bgClip='text'
+					fontFamily={'inherit '}
+					fontStyle={'initial'}
+					fontSize={'5xl'}
+					fontWeight={'100'}
+				>
+					HYDRA
+				</Heading>
+			</HStack>
+			<HStack gap={14} color={'white'} fontSize={'sm'}>
+				<Link href='/About'>ABOUT</Link>
+				<Link href='/Servise'>SERVICES</Link>
+				<Link href='/texnologi'>TECHNOLOGIES</Link>
+				<Link href='/how to'>HOW TO</Link>
+			</HStack>
+			<HStack gap={5}>
+				<Button w={'170px'} h={'40px'} border={'1px'} borderColor={'white'} borderRadius={'xl'}>
+					CONTACT US
+				</Button>
+				<Button w={'160px'} h={'40px'} bgGradient='linear(to-l, #8176AF, #C0B7E8)' borderRadius={'xl'}>
+					{' '}
+					JOIN HYDRA
+				</Button>
+			</HStack>
+		</Box>
+	);
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,17 @@
 import { Box, Heading } from '@chakra-ui/react';
 import React from 'react';
+import { HeaderPage, Users } from '..';
 
 export default function Hero() {
 	return (
-		<Box>
-			<Heading>Hero</Heading>
+		<Box w={'full'} h={'100vh'} bg={'#302C42'} px={'120px'} py={'50px'}>
+			{/* Header page */}
+			<Box>
+				<HeaderPage />
+			</Box>
+			<Box>
+				<Users />
+			</Box>
 		</Box>
 	);
 }
