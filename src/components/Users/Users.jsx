676ce2655681ee3833d9398d2d4adc69ffd46e4a
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-07-29 19:09:25 +0300

    upload users page design next

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,5 +1,6 @@
 import { Box, Button, Heading, HStack, Image, Text } from '@chakra-ui/react';
 import React from 'react';
+import { LuMoveRight } from 'react-icons/lu';
 
 export default function Users() {
 	return (
@@ -27,10 +28,13 @@ export default function Users() {
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore nisl
 					tincidunt eget. Lectus mauris eros in vitae .
 				</Text>
-				<HStack>
-					<Button w={'220px'} h={'50px'} bgGradient='linear(to-l, #8176AF, #C0B7E8)' border={'1px'} borderRadius={'2xl'} mt={5}>
+				<HStack gap={'10'}>
+					<Button w={'220px'} h={'50px'} bgGradient='linear(to-l, #C0B7E8 , #8176AF)' border={'1px'} borderRadius={'2xl'} mt={5}>
 						BUILD YOUR WORLD
 					</Button>
+					<Box pt={5}>
+						<LuMoveRight color='#C0B7E8' size={'50'} />
+					</Box>
 				</HStack>
 			</Box>
 			<Box pt={'80px'}>
